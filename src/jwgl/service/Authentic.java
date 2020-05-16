e34eb9768b0e4b642e7e1939d1e8d8fa96837953
Gopher <wsn0428@sina.com>
2020-05-16 21:43:01 +0800

    Authentic.login

--- src/jwgl/service/Authentic.java
+++ src/jwgl/service/Authentic.java
@@ -1,9 +1,21 @@
 package jwgl.service;
 
+import jwgl.DAO.TheDAO;
+import jwgl.bean.Student;
+
 /**
  * Authentic
  */
 public class Authentic {
 
-    
+    public static boolean login(Student student) {
+        String classID = TheDAO.login(student.getStuID(), student.getPassword());
+
+        if (classID == null)
+            return false;
+        else {
+            student.setClassID(classID);
+            return true;
+        }
+    }
 }
