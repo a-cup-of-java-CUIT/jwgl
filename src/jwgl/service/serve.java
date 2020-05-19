2e3bee03a7a35851762eb66c48ab5bfca3248f8c
Gopher <wsn0428@sina.com>
2020-05-19 21:41:11 +0800

    serve

---
+++ src/jwgl/service/serve.java
@@ -0,0 +1,38 @@
+package jwgl.service;
+
+import java.util.ArrayList;
+
+import jwgl.DAO.TheDAO;
+import jwgl.Entities.Score;
+import jwgl.Entities.Timetable;
+import jwgl.bean.Student;
+
+enum tableTypeEnum {
+    STUDENT_TABLE, CLASS_TABLE
+}
+
+public class serve {
+    public static ArrayList<Timetable> getTimetable(tableTypeEnum tableType, int week, int term, Student student) {
+        ArrayList<Timetable> timetables;
+
+        switch (tableType) {
+            case STUDENT_TABLE:
+                timetables = TheDAO.queryTimetableStudent(student.getStuID(), term);
+                break;
+
+            case CLASS_TABLE:
+                timetables = TheDAO.queryTimetableClass(student.getClassID(), term);
+                break;
+
+            default:
+                timetables = null;
+                break;
+        }
+
+        return timetables;
+    }
+
+    public static ArrayList<Score> getScore(Student student, int term) {
+        return TheDAO.queryScore(student.getStuID(), term);
+    }
+}
